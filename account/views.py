eae6974681ffae29b1aec8df8c52af0deef56efa
perkley <tietjend@byui.edu>
2024-06-06 12:19:12 -0600

    Added more fun pages with graphics

--- account/views.py
+++ account/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
+from django.templatetags.static import static
 
 @login_required
 def account(request):
-    return render(request, 'account/account.html')
+    dietary_restrictions = [
+        {
+            "title": "Dairy",
+            "image_url": static('images/allergy/dairy.png'),
+            "is_selected": True
+        },
+        {
+            "title": "Peanuts",
+            "image_url": static('images/allergy/peanuts.png'),
+            "is_selected": False
+        },
+        {
+            "title": "Soy",
+            "image_url": static('images/allergy/soy.png'),
+            "is_selected": False
+        },
+        {
+            "title": "Tree Nuts",
+            "image_url": static('images/allergy/tree_nuts.png'),
+            "is_selected": False
+        },
+        {
+            "title": "Gluten",
+            "image_url": static('images/allergy/wheat.png'),
+            "is_selected": True
+        },
+        {
+            "title": "Fish",
+            "image_url": static('images/allergy/fish.png'),
+            "is_selected": False
+        },
+    ]
+    
+    context = {
+        'dietary_restrictions': dietary_restrictions
+    }
+
+    return render(request, 'account/account.html', context)
