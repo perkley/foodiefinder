f01909a752ba4bfb3f45cf8c325283b324f75629
perkley <tietjend@byui.edu>
2024-06-06 12:29:54 -0600

    Added storages

--- foodiefinder/settings/production.py
+++ foodiefinder/settings/production.py
@@ -65,30 +65,6 @@ AWS_STORAGE_BUCKET_NAME = secrets['S3BucketName']
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}  # Optional: Cache static files for a day
 
-# # STORAGES Dictionary
-# STORAGES = {
-#     'default': {
-#         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-#         'OPTIONS': {
-#             'AWS_STORAGE_BUCKET_NAME': AWS_STORAGE_BUCKET_NAME,
-#             'AWS_S3_CUSTOM_DOMAIN': AWS_S3_CUSTOM_DOMAIN,
-#             'AWS_S3_OBJECT_PARAMETERS': AWS_S3_OBJECT_PARAMETERS,
-#         }
-#     },
-#     'staticfiles': {
-#         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-#         'OPTIONS': AWS_STORAGE_BUCKET_NAME.rstrip('/') + '/static',
-#     },
-#     'mediafiles': {
-#         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
-#         'OPTIONS': AWS_STORAGE_BUCKET_NAME.rstrip('/') + '/media',
-#     },
-# }
-
-# # Static Files (using 'staticfiles' storage from STORAGES)
-# STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
-
-
 STORAGES = {
     'default': {
         'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage',
@@ -120,8 +96,6 @@ STORAGES = {
 
 # Static Files (using 'staticfiles' storage from STORAGES)
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Media Files (using 'mediafiles' storage from STORAGES)
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
